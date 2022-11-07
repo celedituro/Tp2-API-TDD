274df2d4897c0496c0d7b93c41ddeca54e8c7027
vickyylopezz <vlopez@fi.uba.ar>
2022-11-07 20:42:30 -0300

    feat: agregamos escenarios para consultar menus

---
+++ features/step_definitions/consultar_menus_steps.rb
@@ -0,0 +1,11 @@
+Dado(/^que soy un usuario registrado$/) do
+  pending
+end
+
+Cuando(/^realizo un "([^"]*)" a "([^"]*)"$/) do |metodo_http, comando|
+  pending
+end
+
+Entonces(/^recibo \[\{id:"([^"]*)", nombre:"([^"]*)", precio: "([^"]*)"\},\{id:"([^"]*)", nombre:"([^"]*)", precio: "([^"]*)"\},\{id:"([^"]*)", nombre:"([^"]*)", precio: "([^"]*)"\}\] como response$/) do |id_1, nombre_1, precio_1, id_2, nombre_2, precio_2, id_3, nombre_3, precio_3|
+  pending
+end
