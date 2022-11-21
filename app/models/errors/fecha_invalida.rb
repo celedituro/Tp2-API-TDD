ec9fc717502ba091ad3cab1d7307ac08b41dd42a
celedituro <cdituro@fi.uba.ar>
2022-11-21 20:18:19 -0300

    feat: agregamos validacion de fecha valida para obtener tiempo

---
+++ app/models/errors/fecha_invalida.rb
@@ -0,0 +1,2 @@
+class FechaInvalida < StandardError
+end
--- spec/app/models/servicio_tiempo_spec.rb
+++ spec/app/models/servicio_tiempo_spec.rb
@@ -4,13 +4,19 @@ describe ServicioTiempo do
 
     it 'debe devolver 0 si no llovio en la fecha indicada' do
       fecha = Date.new(2022,11,20)
-      mock_servicio_tiempo = instance_double('servicio_tiempo')
-      allow(mock_servicio_tiempo).to receive(:obtener_factor_tiempo).with(fecha).and_return(0)
+      api_tiempo = instance_double('servicio_tiempo')
+      allow(api_tiempo).to receive(:obtener_factor_tiempo).with(fecha).and_return(0)
     end
 
     it 'debe devolver 0.01 si llovio en la fecha indicada' do
         fecha = Date.new(2022,11,19)
         mock_servicio_tiempo = instance_double('servicio_tiempo')
         allow(mock_servicio_tiempo).to receive(:obtener_factor_tiempo).with(fecha).and_return(0.01)
+    end
+
+    it 'debe devolver fecha_invalida.rb si la diferencia de dias entre la actual y la fecha es mayor a 7' do
+      fecha = Date.new(2022,11,13)
+      mock_servicio_tiempo = instance_double('servicio_tiempo')
+      allow(mock_servicio_tiempo).to receive(:obtener_factor_tiempo).and_raise(FechaInvalida.new)
     end
 end
